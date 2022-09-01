ead59e75f02b46ad30b763d010107efeee299edf
jusong23 <jusong23@naver.com>
2022-09-01 13:00:53 +0900

    Search Setting OK

--- RigingTest_Bunjang/Action/Search/SearchVC.swift
+++ RigingTest_Bunjang/Action/Search/SearchVC.swift
@@ -12,18 +12,32 @@ class SearchVC: UIViewController {
     var Searches = get_7_2_search ()
     var recommandBrandDataModel = RecommandBrandListDataModel ()
 
+    @IBOutlet weak var SearchWord: UILabel!
+    
+    @IBOutlet weak var LabelView: UIView!
     @IBOutlet var UnderView: UIView!
     @IBOutlet var TopView: UIView!
     @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var TopAnchorForMainView: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
-        
+            
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UINib(nibName: "RcmdCell", bundle: .main), forCellReuseIdentifier: "RcmdCell")
         gettingSearch()
+        InitialSearch()
+    }
+    
+    func aboutLabel() {
+        self.SearchWord.sizeToFit()
+    }
+    
+    func InitialSearch() {
+        self.TopAnchorForMainView.constant = 0
+        self.LabelView.isHidden = true
     }
     
     func gettingSearch() {
@@ -44,7 +58,12 @@ class SearchVC: UIViewController {
                         isFollowing: result.baseResult.brandNameList[i].isFollowing)
                 }
                 
-                print(result.baseResult.brandNameList.count)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                    
+                    self.SearchWord.text = result.baseResult.searchList.last?.searchText
+                }
                 
             case let .failure(error):
                 debugPrint("error \(error)")
@@ -75,11 +94,26 @@ extension SearchVC: UITableViewDataSource, UITableViewDelegate {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.recommandBrandDataModel.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "RcmdCell", for: indexPath) as? RcmdCell else { return UITableViewCell() }
+
+        var cellBrandName = self.recommandBrandDataModel.getBrandName(index: indexPath.row)
+        var cellBrandSubName = self.recommandBrandDataModel.getBrandSubName(index: indexPath.row)
+        var cellImageUrl = self.recommandBrandDataModel.getImageUrl(index: indexPath.row)
+        var cellBrandItemCnt = self.recommandBrandDataModel.getBrandItemCnt(index: indexPath.row)
+        
+        cell.brandName.text = cellBrandName
+        cell.brandSubName.text = cellBrandSubName
+        cell.brandItemCnt.text = cellBrandItemCnt
+        
+        var url = URL(string: cellImageUrl ?? "")
+        
+        var fakeUrl = URL(string: "https://cdn1.domeggook.com/upload/item/2022/08/17/1660728672D2FC60FB94167B9A7FBEE4/1660728672D2FC60FB94167B9A7FBEE4_stt_150.png?hash=c816d722ffe0ddd7f0f464b7056047fc")
+        
+        cell.brandImage.load_7_3(url_7_3: (url ?? fakeUrl)!)
 
         return cell
     }
@@ -94,5 +128,19 @@ extension SearchVC : SearchTabBarControllerDelegate {
         guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "SearchVC") else {return}
                viewController.modalPresentationStyle = .fullScreen
                self.present(viewController, animated: true, completion: nil)
+    }
+}
+
+extension UIImageView {
+    func load_7_3(url_7_3: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url_7_3) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
     }
 }
