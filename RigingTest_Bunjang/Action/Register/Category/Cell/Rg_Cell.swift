db4241f8e17fe74cc37e0f8b557b2cc647baac0d
jusong23 <jusong23@naver.com>
2022-09-02 01:26:00 +0900

    Register UI OK

--- RigingTest_Bunjang/Action/Register/Category/Cell/Rg_Cell.swift
+++ RigingTest_Bunjang/Action/Register/Category/Cell/Rg_Cell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class Rg_Cell: UITableViewCell {
 
+    @IBOutlet weak var Category: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- RigingTest_Bunjang/Action/Register/Category/Rg_Category.swift
+++ RigingTest_Bunjang/Action/Register/Category/Rg_Category.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class Rg_Category: UIViewController {
     
+    var CategoryList = ["여성의류","남성의류","신발","가방","시계/주얼리","패션 액세서리","디지털/가전","스포츠/레저","차량/오토바이","스타굿즈","키덜트","에술/희귀/수집품","음반/악기","도서/티켓/문구","뷰티/미용","가구/인테리어","생활/가공식품","유아동/출산","반려동물용품","기타","지역 서비스","원룸/함께살아요","번개니눔","구인구직","재능","커뮤니티"]
+    
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -34,7 +36,7 @@ extension Rg_Category: UITableViewDataSource, UITableViewDelegate {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return self.CategoryList.count
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
@@ -47,6 +49,8 @@ extension Rg_Category: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Rg_Cell", for: indexPath) as? Rg_Cell else { return UITableViewCell() }
+        
+        cell.Category.text = self.CategoryList[indexPath.row]
         
         return cell
     }
